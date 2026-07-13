4751026665248d573773054f97d5b345ac621946
podscapelabs <podscapelabs@gmail.com>
2026-07-13 12:42:22 -0400

    Adopt PodBound Field terminology

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export async function Header() {
         <Link className="brand-lockup" href="/" aria-label="PodBound home"><span className="isopod-crop header-isopod"><Image src="/assets/logos/podbound-isopod.png" alt="" width={1024} height={450} priority /></span><span className="wordmark">PodBound™</span></Link>
         <nav aria-label="Primary navigation">
           <Link href="/#about">About</Link>
-          <Link href="/arena">Arena</Link>
+          <Link href="/arena">PodBound Field</Link>
           {user ? <Link href="/account">Account</Link> : <Link href="/sign-in">Sign in</Link>}
           {!user && <Link className="nav-cta" href="/register">Register</Link>}
         </nav>
